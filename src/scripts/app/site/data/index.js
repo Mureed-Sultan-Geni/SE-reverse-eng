a2e00724b5a191aa577f513415ae76fa65332f7c
Mureed Sultan <mureedsultangeni@gmail.com>
2022-06-29 14:35:35 +0500

    header complete

---
+++ src/scripts/app/site/data/index.js
@@ -0,0 +1,115 @@
+const data = {
+  item1: {
+    img: '../../../../images/stateempire/cube.jpg',
+    title: {
+      logo: 'cube',
+      det: 'soon',
+    },
+    desc: 'Web 3.0 integration, payment SDKs',
+  },
+  item2: {
+    img: '../../../../images/stateempire/thejack.jpg',
+    title: {
+      logo: 'Jacky Winter',
+      det: `'22`,
+    },
+    desc: 'Custom API front/Backend development',
+  }, 
+  item3: {
+    img: '../../../../images/stateempire/artplay.jpg',
+    title: {
+      logo: 'Arts Play',
+      det: `'21`,
+    },
+    desc: 'Custom API Headless CMS, payment solutions',
+  },  
+  item4: {
+    img: '../../../../images/stateempire/jackwinter.jpg',
+    title: {
+      logo: 'VIP',
+      det: `'21`,
+    },
+    desc: 'Headless, CMS, airtable, Tabs!',
+  },
+  item5: {
+    img: '../../../../images/stateempire/flutter.gif',
+    title: {
+      logo: 'Flutter',
+      det: `'21`,
+    },
+    desc: 'Full Stack Development',
+  },  
+  item6: {
+    img: '../../../../images/stateempire/article.jpg',
+    title: {
+      logo: 'Hoverboard Finance',
+      det: `'21`,
+    },
+    desc: 'Headleass CMS, Air table, EDMs',
+  },  
+  item7: {
+    img: '../../../../images/stateempire/example.jpg',
+    title: {
+      logo: 'Modular',
+      det: `'21`,
+    },
+    desc: 'API Development, custom UI, front-end',
+  },  
+  item8: {
+    img: '../../../../images/stateempire/studio.jpg',
+    title: {
+      logo: 'Anatomy Studio',
+      det: `'21`,
+    },
+    desc: 'Headless CMS',
+  },  
+  item9: {
+    img: '../../../../images/stateempire/loadloop.gif',
+    title: {
+      logo: 'open Hours',
+      det: `'20`,
+    },
+    desc: ' ',
+  },  
+  item10: {
+    img: '../../../../images/stateempire/airline.jpg',
+    title: {
+      logo: 'Singapore Air lines',
+      det: `'20`,
+    },
+    desc: 'Full stack development, SSR',
+  },  
+  item11: {
+    img: '../../../../images/stateempire/setjs.jpeg',
+    title: {
+      logo: 'SETJS',
+      det: `'19`,
+    },
+    desc: 'Custom JS Framework',
+  },  
+  item12: {
+    img: '../../../../images/stateempire/swiss.jpg',
+    title: {
+      logo: 'Nichilas Munt',
+      det: `'19`,
+    },
+    desc: ' ',
+  },  
+  item13: {
+    img: '../../../../images/stateempire/house.jpg',
+    title: {
+      logo: 'Jacky Winter',
+      det: `'19`,
+    },
+    desc: 'Full Stack Development, Custom API',
+  },  
+  item14: {
+    img: '../../../../images/stateempire/app.jpg',
+    title: {
+      logo: 'ANZ Banking',
+      det: `'19`,
+    },
+    desc: 'Full Stack Development',
+  },
+};
+export default data;
